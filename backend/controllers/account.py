23fdb64be8ebe116cd85789daf9c71de72782cc1
obadoni2 <obadoniemma@gmail.com>
2024-10-25 01:27:34 +0100

    project

---
+++ backend/controllers/account.py
@@ -0,0 +1,49 @@
+from flask import Blueprint, Response
+from flask import request, session, jsonify
+from model.user import User
+from config import db
+from model.crypto_currency import CryptoCurrencySchema
+from model.crypto_account import CryptoAccount
+from flask_jwt_extended import (
+    jwt_required,
+    get_jwt_identity
+)
+
+account = Blueprint("account", __name__)
+
+
+@account.route("/deposit", methods=["PATCH"])
+@jwt_required()
+def deposit():
+    amount = request.json["amount"]
+    user_id = get_jwt_identity()["id"]
+    user = User.query.get(user_id)
+    payment_card = user.payment_card
+    crypto_account = user.crypto_account
+    if(payment_card.money_amount - int(amount) < 0):
+        return jsonify({"error": "You don't have enough money on payment card"})
+    payment_card.money_amount -= int(amount)
+    crypto_account.amount += int(amount)
+    db.session.commit()
+    return Response(status=200)
+
+@account.route("/getCrypto")  # pregled stanja
+@jwt_required()
+def get_crypto():
+    user_id = get_jwt_identity()["id"]
+    crypto_account = CryptoAccount.query.filter_by(user_id=user_id).first()
+    all_crypto_currencies = crypto_account.crypto_currencies
+    schema = CryptoCurrencySchema(many=True)
+    results = schema.dump(all_crypto_currencies)
+    return jsonify(results)
+
+
+@account.route("/getMoney")  # pregled stanja
+@jwt_required()
+def get_money():
+    #user_id = session.get("user_id")
+    user_id = get_jwt_identity()["id"]
+    crypto_account = CryptoAccount.query.filter_by(user_id=user_id).first()
+
+    return jsonify({"value": crypto_account.amount}), 200
+
--- backend/main.py
+++ backend/main.py
@@ -1,35 +1,38 @@
-from flask import Flask 
-from flask_cors import CORS 
-from flask_session import Session 
-from controllers.auth import auth 
-from controllers.transaction import  transaction 
-from controllers.account import account 
+from flask import Flask
+from flask_cors import CORS
+from flask_session import Session
+from controllers.auth import auth
+from controllers.crypto import crypto
+from controllers.transaction import transaction
+from controllers.account import account
 from datetime import datetime, timedelta
-from  flask import jsonify 
+from flask import jsonify
 from flask_jwt_extended import JWTManager
-from config import db, ma, mail, bcrpt, ApplicationConfig
-
-
+from config import db, ma, mail, bcrypt, ApplicationConfig
 
 app = Flask(__name__)
 app.config.from_object(ApplicationConfig)
-app. register_blueprint(auth, url_prefix="/auth")
+
+app.register_blueprint(auth, url_prefix="/auth")
 app.register_blueprint(crypto, url_prefix="/crypto")
+app.register_blueprint(transaction, url_prefix="/transaction")
 app.register_blueprint(account, url_prefix="/account")
 
-Cors(app,supports_credentials = True)
-#mail = mail(app)#ovo  obrisi ubuduce 
-# enable server side session sve je  na serve sem session  id
+CORS(app, supports_credentials=True)
+
+#mail = Mail(app)#ovo obrisi u buduce
+# enabeld server side seesion sve je na serveru sem session id
 server_session = Session(app)
 jwt = JWTManager(app)
 db.init_app(app)
-bcrtypt.init_app(app)
+ma.init_app(app)
+bcrypt.init_app(app)
 mail.init_app(app)
 
 @app.route("/create")
-def create(): 
+def create():
     db.create_all()
-    return "all table created"
+    return "All tables created"
 
 # @app.route("/authh")
 # def authh():
@@ -42,5 +45,22 @@ def create():
 
 #     return "i need token bro"
 
-if __name__ =="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
